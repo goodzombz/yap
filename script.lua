ca6a0935c3f5a5d396b013f3b3616e10a3f448f4
goodzombz <95709219+goodzombz@users.noreply.github.com>
2024-06-01 04:21:02 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,13 @@
 local result = get("result")
+local text = get("text")
+local button = get("done-3")
+local num = 20
 
-
+button.on-click(function()
+  if text.get_content() > num then
+      result.set_content("lower")
+    end
+  if text.get_content() < num then
+      result.set_content("higher")
+    end
+end)
