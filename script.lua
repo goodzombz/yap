304b92ac1a28de47ab05af7f274da21bf2774c50
goodzombz <95709219+goodzombz@users.noreply.github.com>
2024-06-01 07:54:39 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,10 +5,10 @@ local inp = get("delete-input-key")
 local button2 = get("button2")
 local fjs = get("fjavascript")
 button.on_click(function()
+    fjs.set_content(fjs.get_content() .. "fuck javascript ")
     if tonumber(inp.get_content()) > num then result.set_content("lower") end
     if tonumber(inp.get_content()) < num then result.set_content("higher") end
     if tonumber(inp.get_content()) == num then result.set_content("you win!!!") end
-    fjs.set_content("fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript fuck javascript ")
 end)
 
 button2.on_click(function()
