9369bd175d20dbef3b4984fff07ed5244414e32d
goodzombz <95709219+goodzombz@users.noreply.github.com>
2024-06-01 05:58:51 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ local button = get("button")
 num = 20
 local inp = get("delete-input-key")
 button.on_click(function()
-    if inp.get_content() > num then result.set_content("lower") end
-    if inp.get_content() < num then result.set_content("higher") end
-    if inp.get_content() == num then result.set_content("you win!!!") end
+    if tonumber(inp.get_content()) > num then result.set_content("lower") end
+    if tonumber(inp.get_content()) < num then result.set_content("higher") end
+    if tonumber(inp.get_content()) == num then result.set_content("you win!!!") end
 end)
